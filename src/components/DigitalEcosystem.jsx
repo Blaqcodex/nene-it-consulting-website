08e9df5ad3d4fdb88e3d72edb30d1a48b0dd4eff
Blaqcodex <117004858+Blaqcodex@users.noreply.github.com>
2026-06-11 04:09:58 +0200

    feat: create digital ecosystem section foundation

---
+++ src/components/DigitalEcosystem.jsx
@@ -0,0 +1,37 @@
+export default function DigitalEcosystem() {
+  return (
+    <section className="py-40">
+
+      <div className="max-w-6xl mx-auto px-8 text-center">
+
+        <h2 className="text-6xl font-black mb-20">
+
+          Digital Ecosystem
+
+        </h2>
+
+        <div className="relative h-[500px]">
+
+          <div className="absolute top-0 left-1/2 -translate-x-1/2">
+            AI
+          </div>
+
+          <div className="absolute left-0 top-1/2">
+            Cloud
+          </div>
+
+          <div className="absolute right-0 top-1/2">
+            Software
+          </div>
+
+          <div className="absolute bottom-0 left-1/2 -translate-x-1/2">
+            Business
+          </div>
+
+        </div>
+
+      </div>
+
+    </section>
+  );
+}
