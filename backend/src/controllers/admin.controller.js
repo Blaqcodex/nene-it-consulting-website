86c7cda54e167172b4ee64d6b0cf0ff530d37802
Blaqcodex <117004858+Blaqcodex@users.noreply.github.com>
2026-07-05 16:11:56 +0200

    feat(admin): enhance dashboard to include statistics and recent enquiries

--- backend/src/controllers/admin.controller.js
+++ backend/src/controllers/admin.controller.js
@@ -1,7 +1,43 @@
+import Contact from "../models/contact.model.js";
+
 export const getDashboard = async (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: "Welcome to the Admin Dashboard!",
-    user: req.user,
-  });
+  try {
+    const totalEnquiries = await Contact.countDocuments();
+
+    const newEnquiries = await Contact.countDocuments({
+      status: "New",
+    });
+
+    const contacted = await Contact.countDocuments({
+      status: "Contacted",
+    });
+
+    const closed = await Contact.countDocuments({
+      status: "Closed",
+    });
+
+    const recentEnquiries = await Contact.find()
+      .sort({ createdAt: -1 })
+      .limit(5)
+      .select("-__v");
+
+    res.status(200).json({
+      success: true,
+
+      statistics: {
+        totalEnquiries,
+        newEnquiries,
+        contacted,
+        closed,
+      },
+
+      recentEnquiries,
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
